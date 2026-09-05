adea37793b4eb3d95481db09c940395be8728c16
Hamham <abrahamvero26@gmail.com>
2026-09-05 16:26:49 +0700

    week02: challenge refactor primary constructor using default args

--- src/oop_00000144521_AbrahamNieveroNathanael/week02/Student.kt
+++ src/oop_00000144521_AbrahamNieveroNathanael/week02/Student.kt
@@ -1,6 +1,6 @@
 package oop_00000144521_AbrahamNieveroNathanael.week02
 
-class Student (val name: String, val nim: String, var major: String){
+class Student (val name: String, val nim: String, var major: String, var gpa: Double = 0.0){
     init{
         if(nim.length != 5){
             println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
@@ -9,7 +9,7 @@ class Student (val name: String, val nim: String, var major: String){
             println("Log: Objek student $name berhasil dialokasikan di memory.")
         }
     }
-    constructor(name: String, nim: String) : this(name, nim, "Non-Matriculated") {
+    constructor(name: String, nim: String) : this(name, nim, "Non-Matriculated", gpa = 0.0) {
         println("Log: Menggunakan constructor jalur umum (Tanpa Jurusan).")
     }
 }

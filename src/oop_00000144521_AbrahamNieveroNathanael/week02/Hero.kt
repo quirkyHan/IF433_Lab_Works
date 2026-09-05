05889aae66f2f3c0cbaf3c6899ae6f22b6af1ffd
Hamham <abrahamvero26@gmail.com>
2026-09-05 19:28:49 +0700

    week02: (task 2) create Hero class

---
+++ src/oop_00000144521_AbrahamNieveroNathanael/week02/Hero.kt
@@ -0,0 +1,20 @@
+package oop_00000144521_AbrahamNieveroNathanael.week02
+
+class Hero(val name: String, val baseDamage: Int, var hp: Int = 100){
+
+    fun attack(targetName: String){
+        println("$name menebas $targetName!")
+    }
+
+    fun takeDamage(damage: Int){
+        hp -= damage
+
+        if (hp < 0){
+            hp = 0
+        }
+    }
+
+    fun isAlive(): Boolean{
+        return hp > 0
+    }
+}

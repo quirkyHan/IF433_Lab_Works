85ca7f7e38c3354bc50a7eb7457b7bcb474be887
Hamham <abrahamvero26@gmail.com>
2026-09-05 16:29:14 +0700

    week02: implement selection logic for constructors

--- src/oop_00000144521_AbrahamNieveroNathanael/week02/Main.kt
+++ src/oop_00000144521_AbrahamNieveroNathanael/week02/Main.kt
@@ -23,5 +23,22 @@ fun main() {
 
         val s1 = Student(name, nim, major)
         println("Status pendaftaran selesai.")
+
+        print("Pilih Jalur (1. Reguler, 2. Umum): ")
+        val type = scanner.nextInt()
+        scanner.nextLine()
+
+        if (type == 1){
+            print("Masukkan jurusan: ")
+            val major = scanner.nextLine()
+
+            val s1 = Student(name, nim, major)
+            println("Terdaftar di: ${s1.major} dengan GPA awal ${s1.gpa}")
+        } else if (type == 2){
+            val s2 = Student(name, nim)
+            println("Terdaftar di: ${s2.major} dengan GPA awal ${s2.gpa}")
+        } else {
+            println("Pilihan ngawur, pendaftaran batal!")
+        }
     }
 }

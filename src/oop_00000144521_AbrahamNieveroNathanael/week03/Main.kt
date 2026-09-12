c50ffbe1b21bfb7c3ff2aa755d000f4b91780b9b
Abraham <abrahamvero26@gmail.com>
2026-09-12 11:30:08 +0700

    week03: test all encapsulation logic in main

--- src/oop_00000144521_AbrahamNieveroNathanael/week03/Main.kt
+++ src/oop_00000144521_AbrahamNieveroNathanael/week03/Main.kt
@@ -2,6 +2,11 @@ package oop_00000144521_AbrahamNieveroNathanael.week03
 
 fun main() {
     val e = Employee("Budi")
-    println("Test error")
-    e.salary = 500
+    e.salary = -1000
+    e.salary = 5000000
+    println("gaji: ${e.salary}")
+
+    e.increasePerformace()
+
+    println("Pajak yang harus dibayar: ${e.tax}")
 }

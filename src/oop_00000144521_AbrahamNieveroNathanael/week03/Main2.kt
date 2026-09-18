1dffa2cdcf362dca15999a064cb06cc1f51920c3
Hamham <abrahamvero26@gmail.com>
2026-09-18 18:56:44 +0700

    week03: (task 1) implement Weapon testing in main

---
+++ src/oop_00000144521_AbrahamNieveroNathanael/week03/Main2.kt
@@ -0,0 +1,11 @@
+package oop_00000144521_AbrahamNieveroNathanael.week03
+
+fun main(){
+    val weapon = Weapon("Rudal jawa", 99999)
+
+    weapon.damage = -50
+    weapon.damage = 9999
+    println("weapon damage: ${weapon.damage}")
+    println("tier senjata: ${weapon.tier}")
+
+}

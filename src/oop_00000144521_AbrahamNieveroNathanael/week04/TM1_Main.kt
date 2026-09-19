b8ac836bfb6bbcef491b4e1459c715618aed06bb
Abraham <abrahamvero26@gmail.com>
2026-09-19 10:43:49 +0700

    week04: (task 1) test ElectricCar in main

---
+++ src/oop_00000144521_AbrahamNieveroNathanael/week04/TM1_Main.kt
@@ -0,0 +1,9 @@
+package oop_00000144521_AbrahamNieveroNathanael.week04
+
+fun main(){
+    val ElectricCar = electricCar("BYD", 2, 90)
+
+    ElectricCar.accelerate()
+    ElectricCar.honk()
+    ElectricCar.openTrunk()
+}
